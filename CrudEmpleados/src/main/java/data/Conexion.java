326c991fba3c73473780c99b7d8be596fb609553
CristianBoyacaG07 <99199734+CristianBoyacaG07@users.noreply.github.com>
2022-05-24 00:53:02 -0500

    Add files via upload

---
+++ CrudEmpleados/src/main/java/data/Conexion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data;
+
+import java.sql.*;
+
+public class Conexion {
+    Connection con;
+    public Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/registro","root","12345678");            
+        } catch (Exception e) {
+            System.err.println("Error"+e);
+        }
+    }
+    public Connection getConnection(){
+        return con;
+    }
+}
---
+++ CrudEmpleados/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<header>Registro Empleados</header>
+	<div>
+        <a href="ServletEmpleadosControlador?accion=read" > Empleados</a> <%--read --%>
+		<a href="ServletDepartamentosControlador?accion=read" >Departamentos</a> <%--read --%>
+</div>
+</body>
+</html>
